f66256454288a69998a4289cf166ef7aaa6eb58c
moringawonder <adams.wonderboy@student.moringaschool.com>
2022-08-21 23:30:11 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,5 @@
+let main=document.querySelector('main')
+main.remove()
+let newHeader=document.createElement("h1")
+newHeader.setAttribute("id","victory")
+newHeader.innerHTML="YOUR-NAME is the champion"
